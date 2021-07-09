491d7a819e5721df4e4639ac3f3f09496c282088
Mehedi Hasan <mhasan172036@mscse.uiu.ac.bd>
2021-07-10 04:25:01 +0600

    2nd commit

--- src/task.java
+++ src/task.java
@@ -1,6 +1,14 @@
 public class task{
     public static void main(String[] args) {
         String mes = "Hello world!";
+        task obj = new task();
+        obj.add();
         System.out.println(mes);
+    }
+    public void add(){
+        int a =10;
+        int b = 6;
+        int sum = a+b;
+        System.out.println(sum);
     }
 }
